e4671ab5135b52a49746b4b57696e6329bbf3fb2
Natallia <natalisnataliasotskaja@gmail.com>
2018-01-02 10:26:01 +0200

    Refactor, changed content

--- public/js/includes/script.js
+++ public/js/includes/script.js
@@ -1,21 +1,122 @@
-// (function ($) {
-//     'use strict';
-//
-//     var $popup         = $('.popup');
-//     var $popupBackdrop = $('.popup-backdrop');
-//
-//     $(document).ready(function () {
-//         $popupBackdrop.on('click', function () {
-//             $popup.toggleClass('hide');
-//             $popupBackdrop.toggleClass('hide');
-//         });
-//     });
-//
-//     $(document).on('click', '[data-popup-id]', function () {
-//         var $popupId = $(this).attr('data-popup-id');
-//
-//         $('#' + $popupId).toggleClass('hide');
-//         $popupBackdrop.toggleClass('hide');
-//     });
-//
-// })(jQuery);
+(function ($) {
+    'use strict';
+
+    var App = {
+        SMALL_SCREEN_WIDTH: 540,
+        MEDIUM_SCREEN_WIDTH: 768,
+        LARGE_SCREEN_WIDTH: 1024,
+        XLARGE_SCREEN_WIDTH: 1200
+    };
+
+    window.debounce = function(func, ms) {
+
+        var shouldExec = true;
+
+        return function() {
+            if (!shouldExec) {
+                return;
+            }
+
+            func.apply(this, arguments);
+
+            shouldExec = false;
+
+            setTimeout(function() { shouldExec = true; }, ms);
+        };
+    };
+
+    $(document).ready(function () {
+
+        // ----- Fixed section -----
+
+        var $fixedSection = $('.fixedSection');
+        var $fixedSectionContent = $fixedSection.find('.fixedSectionContent');
+        var $fixedSectionPlaceholder = $fixedSection.find('.fixedSectionPlaceholder');
+
+        function makeFixedSection() {
+
+            if (window.innerWidth > App.MEDIUM_SCREEN_WIDTH) {
+                $fixedSectionContent.css({
+                    'position': 'fixed',
+                    'top': 0,
+                    'left': 0,
+                    'right': 0,
+                    'z-index': 0
+                });
+
+                $fixedSectionPlaceholder.css({
+                    'height': $fixedSectionContent.outerHeight(true)
+                });
+            } else {
+                $fixedSectionContent.css({
+                    'position': 'relative'
+                });
+
+                $fixedSectionPlaceholder.css({
+                    'height': 'auto'
+                });
+            }
+        }
+
+        if ($fixedSection && $fixedSectionContent && $fixedSectionPlaceholder) {
+
+            makeFixedSection();
+
+            $(window).on('resize', debounce(makeFixedSection, 200));
+        }
+
+        // ----- Header -----
+
+        var $html = $('html');
+        var $headerMenuMobileButton = $('.hamburgerBtn');
+        var $headerMenu = $('.nav-wrap');
+
+        $(window).on('load resize', function () {
+
+            if ($headerMenuMobileButton.hasClass('is-active') && $html.hasClass('overflow-hidden')) {
+                $headerMenuMobileButton.removeClass('is-active');
+                $html.removeClass('overflow-hidden');
+                $headerMenu.slideUp('fast');
+            }
+        });
+
+        $headerMenuMobileButton.on('click', function (e) {
+            e.preventDefault();
+            $(this).toggleClass('is-active');
+            $html.toggleClass('overflow-hidden');
+            $headerMenu.slideToggle('fast');
+        });
+
+        // ----- Libs initialization -----
+
+        var $phoneNumber = $('#phoneNumber');
+
+        if ($phoneNumber) {
+            $phoneNumber.inputmask({ "mask": "+3 (80) 9999-99999" });
+        }
+
+        $(function () {
+            $('select').selectric();
+        });
+
+        // ----- Modal -----
+
+        // var $popup         = $('.popup');
+        // var $popupBackdrop = $('.popup-backdrop');
+        //
+        // $(document).ready(function () {
+        //     $popupBackdrop.on('click', function () {
+        //         $popup.toggleClass('hide');
+        //         $popupBackdrop.toggleClass('hide');
+        //     });
+        // });
+        //
+        // $(document).on('click', '[data-popup-id]', function () {
+        //     var $popupId = $(this).attr('data-popup-id');
+        //
+        //     $('#' + $popupId).toggleClass('hide');
+        //     $popupBackdrop.toggleClass('hide');
+        // });
+    });
+
+})(jQuery);
--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,33 @@
-var App = {
-    SMALL_SCREEN_WIDTH: 540,
-    MEDIUM_SCREEN_WIDTH: 768,
-    LARGE_SCREEN_WIDTH: 1024,
-    XLARGE_SCREEN_WIDTH: 1200
-};
-
-window.debounce = function(func, ms) {
-
-    var shouldExec = true;
-
-    return function() {
-        if (!shouldExec) {
-            return;
-        }
-
-        func.apply(this, arguments);
-
-        shouldExec = false;
-
-        setTimeout(function() { shouldExec = true; }, ms);
-    };
-};
 (function ($) {
     'use strict';
 
+    var App = {
+        SMALL_SCREEN_WIDTH: 540,
+        MEDIUM_SCREEN_WIDTH: 768,
+        LARGE_SCREEN_WIDTH: 1024,
+        XLARGE_SCREEN_WIDTH: 1200
+    };
+
+    window.debounce = function(func, ms) {
+
+        var shouldExec = true;
+
+        return function() {
+            if (!shouldExec) {
+                return;
+            }
+
+            func.apply(this, arguments);
+
+            shouldExec = false;
+
+            setTimeout(function() { shouldExec = true; }, ms);
+        };
+    };
+
     $(document).ready(function () {
+
+        // ----- Fixed section -----
 
         var $fixedSection = $('.fixedSection');
         var $fixedSectionContent = $fixedSection.find('.fixedSectionContent');
@@ -32,12 +35,13 @@ window.debounce = function(func, ms) {
 
         function makeFixedSection() {
 
-            if(window.innerWidth > App.MEDIUM_SCREEN_WIDTH) {
+            if (window.innerWidth > App.MEDIUM_SCREEN_WIDTH) {
                 $fixedSectionContent.css({
                     'position': 'fixed',
                     'top': 0,
                     'left': 0,
-                    'right': 0
+                    'right': 0,
+                    'z-index': 0
                 });
 
                 $fixedSectionPlaceholder.css({
@@ -54,74 +58,65 @@ window.debounce = function(func, ms) {
             }
         }
 
-        if($fixedSection && $fixedSectionContent && $fixedSectionPlaceholder) {
+        if ($fixedSection && $fixedSectionContent && $fixedSectionPlaceholder) {
 
             makeFixedSection();
 
-            $(window).on('resize', debounce(makeFixedSection, 150));
-        }
-    });
-
-})(jQuery);
-(function ($) {
-    'use strict';
-
-    var $html = $('html');
-    var $headerMenuMobileButton = $('.hamburgerBtn');
-    var $headerMenu = $('.nav-wrap');
-
-    $(window).on('load resize', function () {
-
-        if ($headerMenuMobileButton.hasClass('is-active') && $html.hasClass('overflow-hidden')){
-            $headerMenuMobileButton.removeClass('is-active');
-            $html.removeClass('overflow-hidden');
-            $headerMenu.slideUp('fast');
-        }
-    });
-
-    $headerMenuMobileButton.on('click', function (e) {
-        e.preventDefault();
-        $(this).toggleClass('is-active');
-        $html.toggleClass('overflow-hidden');
-        $headerMenu.slideToggle('fast');
-    });
-})(jQuery);
-
-(function ($) {
-    'use strict';
-
-    var $phoneNumber = $('#phoneNumber');
-
-    $(document).ready(function () {
-
-        if($phoneNumber) {
-            $phoneNumber.inputmask({"mask": "+3 (80) 9999-99999"});
+            $(window).on('resize', debounce(makeFixedSection, 200));
         }
 
-        $(function() {
+        // ----- Header -----
+
+        var $html = $('html');
+        var $headerMenuMobileButton = $('.hamburgerBtn');
+        var $headerMenu = $('.nav-wrap');
+
+        $(window).on('load resize', function () {
+
+            if ($headerMenuMobileButton.hasClass('is-active') && $html.hasClass('overflow-hidden')) {
+                $headerMenuMobileButton.removeClass('is-active');
+                $html.removeClass('overflow-hidden');
+                $headerMenu.slideUp('fast');
+            }
+        });
+
+        $headerMenuMobileButton.on('click', function (e) {
+            e.preventDefault();
+            $(this).toggleClass('is-active');
+            $html.toggleClass('overflow-hidden');
+            $headerMenu.slideToggle('fast');
+        });
+
+        // ----- Libs initialization -----
+
+        var $phoneNumber = $('#phoneNumber');
+
+        if ($phoneNumber) {
+            $phoneNumber.inputmask({ "mask": "+3 (80) 9999-99999" });
+        }
+
+        $(function () {
             $('select').selectric();
         });
+
+        // ----- Modal -----
+
+        // var $popup         = $('.popup');
+        // var $popupBackdrop = $('.popup-backdrop');
+        //
+        // $(document).ready(function () {
+        //     $popupBackdrop.on('click', function () {
+        //         $popup.toggleClass('hide');
+        //         $popupBackdrop.toggleClass('hide');
+        //     });
+        // });
+        //
+        // $(document).on('click', '[data-popup-id]', function () {
+        //     var $popupId = $(this).attr('data-popup-id');
+        //
+        //     $('#' + $popupId).toggleClass('hide');
+        //     $popupBackdrop.toggleClass('hide');
+        // });
     });
 
 })(jQuery);
-// (function ($) {
-//     'use strict';
-//
-//     var $popup         = $('.popup');
-//     var $popupBackdrop = $('.popup-backdrop');
-//
-//     $(document).ready(function () {
-//         $popupBackdrop.on('click', function () {
-//             $popup.toggleClass('hide');
-//             $popupBackdrop.toggleClass('hide');
-//         });
-//     });
-//
-//     $(document).on('click', '[data-popup-id]', function () {
-//         var $popupId = $(this).attr('data-popup-id');
-//
-//         $('#' + $popupId).toggleClass('hide');
-//         $popupBackdrop.toggleClass('hide');
-//     });
-//
-// })(jQuery);
